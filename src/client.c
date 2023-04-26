ba04707c223f08081c38ca6faa292abcc20169d9
Nipostni <awis@me.com>
2023-04-26 16:44:02 +0200

    deconstructing char

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: nipostni <awis@me.com>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 13:25:23 by nipostni          #+#    #+#             */
-/*   Updated: 2023/04/24 17:19:40 by nipostni         ###   ########.fr       */
+/*   Updated: 2023/04/26 16:43:02 by nipostni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,46 @@
 #include "include/ft_printf.h"
 #include "include/libft.h"
 
+int char_to_bit(char c, int bits[8])
+{
+	int i;
+
+	i = 0;
+	while (i < 8)
+	{
+		bits[i] = (c >> i) & 1;
+		i++;
+	}
+	return (0);
+}
+	
+
+
 int main(int argc, char *argv[])
 {
+	int bits[8];
+	char test_char = argv[1][0];
     if (argc != 2)
     {
         printf("Usage: %s <server_pid>\n", argv[0]);
         return 1;
     }
 
-    pid_t server_pid = atoi(argv[1]);
+	char_to_bit(test_char, bits);
+	int i = 0;
+	while (i < 8)
+	{
+		printf("%d", bits[i]);
+		i++;
+	}
+    // pid_t server_pid = atoi(argv[1]);
 
-    if (kill(server_pid, SIGUSR1) == -1)
-    {
-        perror("Error sending signal");
-        return 2;
-    }
+    // if (kill(server_pid, SIGUSR1) == -1)
+    // {
+    //     perror("Error sending signal");
+    //     return 2;
+    // }
 
-    printf("Signal sent successfully\n");
+    // printf("Signal sent successfully\n");
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: nipostni <awis@me.com>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 13:25:25 by nipostni          #+#    #+#             */
-/*   Updated: 2023/04/24 17:17:45 by nipostni         ###   ########.fr       */
+/*   Updated: 2023/04/26 16:27:14 by nipostni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,30 @@ void signal_handler(int sig)
 	ft_printf("Signal received: %d\n", sig);
 }
 
+char bits_to_char(int bits[8])
+{
+	char c;
+	int i;
+
+	c = 0;
+	i = 0;
+
+	while (i < 8)
+	{
+		c += bits[i] << i;
+		i++;
+	}
+	return (c);
+}
+
 int main(void)
 {
+	// int bits[8];
 	pid_t pid;
 	pid = getpid();
 	ft_printf("PID: %d\n", pid);
+	int bits[8] = {1,0,0,0,0,0,1,0};
+	printf("%c\n", bits_to_char(bits));
 	signal(SIGUSR1, signal_handler);
 	while (1)
 	{
